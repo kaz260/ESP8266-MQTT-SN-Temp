093b24cf7e744115d5b87f8b56e127530c547f1a
Matsuda, Kazuhiro <kaz.m@tea.ocn.ne.jp>
2016-04-15 15:51:59 +0900

    bug fix
    I fixed the problem that UdpPort::multicast won't send any packet.

--- libraries/AsyncMQTTSN/NetworkWiFi.cpp
+++ libraries/AsyncMQTTSN/NetworkWiFi.cpp
@@ -3,7 +3,6 @@
  *                      The BSD License
  *
  *           Copyright (c) 2015, tomoaki@tomy-tech.com
- *                    All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification,
  * are permitted provided that the following conditions are met:
@@ -208,7 +207,7 @@ int UdpPort::unicast(const uint8_t* buf, uint32_t length, uint32_t ipAddress, ui
 
 
 int UdpPort::multicast( const uint8_t* buf, uint32_t length ){
-	_udpMulticast.beginPacket(_gIpAddr, _gPortNo);
+	_udpMulticast.beginPacketMulticast(_gIpAddr, _gPortNo, _cIpAddr);
 	_udpMulticast.write(buf, length);
 	return _udpMulticast.endPacket();
 }
